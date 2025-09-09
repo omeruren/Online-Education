fd234e51adb66c09e47d8c386dee9b58d956d402
omeruren <omer.uren19@gmail.com>
2025-09-09 23:01:09 +0300

    Added  Controller, Mapping and Dto file for Message Entity

---
+++ OnlineEdu.API/Controllers/MessagesController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Business.Abstract;
+using OnlineEducation.DTO.DTOs.MessageDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController(IGenericService<Message> _messageService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _messageService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _messageService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _messageService.TDelete(id);
+            return Ok("Message entitiy deleted");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateMessageDto createMessageDto)
+        {
+            var newValue = _mapper.Map<Message>(createMessageDto);
+            _messageService.TCreate(newValue);
+            return Ok("Message entitiy created");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateMessageDto updateMessageDto)
+        {
+            var values = _mapper.Map<Message>(updateMessageDto);
+            _messageService.TUpdate(values);
+            return Ok("Message entitiy updated");
+        }
+    }
+}
---
+++ OnlineEdu.API/Mapping/MessageMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OnlineEducation.DTO.DTOs.MessageDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Mapping
+{
+    public class MessageMapping : Profile
+    {
+        public MessageMapping()
+        {
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+        }
+    }
+}
