7dcf997129d1523ef38ba8c5c50da73b9dec26cd
omeruren <omer.uren19@gmail.com>
2025-09-09 23:31:13 +0300

    Added  Controller, Mapping and Dto file for Testimonial Entity

---
+++ OnlineEdu.API/Controllers/TestimonialsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Business.Abstract;
+using OnlineEducation.DTO.DTOs.TestimonialDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController(IGenericService<Testimonial> _testimonialService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _testimonialService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _testimonialService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _testimonialService.TDelete(id);
+            return Ok("Testimonial entitiy deleted");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTestimonialDto createTestimonialDto)
+        {
+            var newValue = _mapper.Map<Testimonial>(createTestimonialDto);
+            _testimonialService.TCreate(newValue);
+            return Ok("Testimonial entitiy created");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var values = _mapper.Map<Testimonial>(updateTestimonialDto);
+            _testimonialService.TUpdate(values);
+            return Ok("Testimonial entitiy updated");
+        }
+    }
+}
---
+++ OnlineEdu.API/Mapping/TestimonialMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OnlineEducation.DTO.DTOs.TestimonialDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Mapping
+{
+    public class TestimonialMapping:Profile
+    {
+        public TestimonialMapping()
+        {
+            CreateMap<CreateTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<UpdateTestimonialDto, Testimonial>().ReverseMap();
+
+        }
+    }
+}
