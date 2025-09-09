dca00d058d87a2f12544a87c8cb80a129ad510e7
omeruren <omer.uren19@gmail.com>
2025-09-09 23:21:04 +0300

    Added  Controller, Mapping and Dto file for Subscriber Entity

---
+++ OnlineEdu.API/Controllers/SubscribersController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Business.Abstract;
+using OnlineEducation.DTO.DTOs.SubsciberDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribersController(IGenericService<Subscriber> _subscriberService, IMapper _mapper) : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _subscriberService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _subscriberService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _subscriberService.TDelete(id);
+            return Ok("Subscriber entitiy deleted");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSubscriberDto createSubscriberDto)
+        {
+            var newValue = _mapper.Map<Subscriber>(createSubscriberDto);
+            _subscriberService.TCreate(newValue);
+            return Ok("Subscriber entitiy created");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateSubscriberDto updateSubscriberDto)
+        {
+            var values = _mapper.Map<Subscriber>(updateSubscriberDto);
+            _subscriberService.TUpdate(values);
+            return Ok("Subscriber entitiy updated");
+        }
+    }
+}
---
+++ OnlineEdu.API/Mapping/SubscriberMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OnlineEducation.DTO.DTOs.SubsciberDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Mapping
+{
+    public class SubscriberMapping: Profile
+    {
+        public SubscriberMapping()
+        {
+            CreateMap<CreateSubscriberDto, Subscriber>().ReverseMap();
+            CreateMap<UpdateSubscriberDto, Subscriber>().ReverseMap();
+        }
+    }
+}
