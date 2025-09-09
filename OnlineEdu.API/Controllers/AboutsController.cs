395b36ed342c96b33afd01347dd33f414a7d6225
omeruren <omer.uren19@gmail.com>
2025-09-09 15:10:19 +0300

    Added Mapper and created About Controller

--- OnlineEdu.API/Controllers/AboutsController.cs
+++ OnlineEdu.API/Controllers/AboutsController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Business.Abstract;
+using OnlineEducation.DTO.DTOs.AboutDtos;
+using OnlineEducation.Entity.Entities;
 
 namespace OnlineEducation.API.Controllers
 {
-    public class AboutsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsController(IGenericService<About> _aboutService, IMapper _mapper) : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Get()
         {
-            return View();
+            var values = _aboutService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _aboutService.TDelete(id);
+            return Ok("About entitiy deleted");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateAboutDto createAboutDto)
+        {
+            var newValue = _mapper.Map<About>(createAboutDto);
+            _aboutService.TCreate(newValue);
+            return Ok("About entitiy created");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateAboutDto updateAboutDto)
+        {
+            var values = _mapper.Map<About>(updateAboutDto);
+            _aboutService.TUpdate(values);
+            return Ok("About entitiy updated");
         }
     }
 }
---
+++ OnlineEdu.API/Mapping/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OnlineEducation.DTO.DTOs.AboutDtos;
+using OnlineEducation.Entity.Entities;
+
+namespace OnlineEducation.API.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<CreateAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+        }
+    }
+}
