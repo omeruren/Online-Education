4cf07b3ffd272ecc97c9ac4739d211318648b93a
omeruren <omer.uren19@gmail.com>
2025-09-10 12:14:23 +0300

    Fetched Data to About Page
    Added HttpClientHelper method to avoid code clutter.So, for all future requests, it will be sufficient to provide only the endpoint path (such as 'products', 'categories').

--- OnlineEducation.UI/Areas/Admin/Controllers/AboutController.cs
+++ OnlineEducation.UI/Areas/Admin/Controllers/AboutController.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.UI.DTOs.AboutDtos;
+using OnlineEducation.UI.Helpers;
+using System.Threading.Tasks;
 
 namespace OnlineEducation.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("[area]/[controller]/[action]/{id?}")]
-    public class AboutController(HttpClient _client) : Controller
+    public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+
+        public async Task<IActionResult> Index()
         {
-            //var values = _client.GetFromJsonAsync();
-            return View();
+
+            var values = await _client.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
+            return View(values);
         }
     }
 }
