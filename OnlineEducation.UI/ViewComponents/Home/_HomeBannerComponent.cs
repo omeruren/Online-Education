c134471374d911d019f6bb346b7f689da06f6bf7
omeruren <omer.uren19@gmail.com>
2025-09-11 21:46:51 +0300

    Added Partial View Component

---
+++ OnlineEducation.UI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEducation.UI.ViewComponents.Home
+{
+    public class _HomeBannerComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
