f215b1c08932cc72c1dc6f7a91e4c226ea7f064a
omeruren <omer.uren19@gmail.com>
2025-09-11 17:05:55 +0300

    Added Course Controller.
    Added Create, Update and Delete logics.

---
+++ OnlineEducation.UI/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OnlineEducation.UI.DTOs.CourseCategoryDtos;
+using OnlineEducation.UI.DTOs.CourseDtos;
+using OnlineEducation.UI.Helpers;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class CourseController : Controller
+    {
+        public async Task CourseCategoryDropDown()
+        {
+            var courseCategoryList = await _client.GetFromJsonAsync<List<ResultCourseCategoryDto>>("courseCategories");
+
+            List<SelectListItem> courseCategories = (from c in courseCategoryList
+                                               select new SelectListItem
+                                               {
+                                                   Text = c.CategoryName,
+                                                   Value = c.CourseCategoryId.ToString()
+                                               }).ToList();
+            ViewBag.courseCategories = courseCategories;
+        }
+
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+
+            var values = await _client.GetFromJsonAsync<List<ResultCourseDto>>("Courses");
+            return View(values);
+        }
+
+        public async Task<ActionResult> CreateCourse()
+        {
+            await CourseCategoryDropDown();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCourse(CreateCourseDto createCourseDto)
+        {
+
+            await _client.PostAsJsonAsync("Courses", createCourseDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateCourse(int id)
+        {
+            await CourseCategoryDropDown();
+            var values = await _client.GetFromJsonAsync<UpdateCourseDto>($"Courses/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCourse(UpdateCourseDto updateCourseDto)
+        {
+            var values = await _client.PutAsJsonAsync("Courses", updateCourseDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            await _client.DeleteAsync($"Courses/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+    
+    }
+}
