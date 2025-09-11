deb57977ccf3e3a883b3e81414bb8b4daa0ab222
omeruren <omer.uren19@gmail.com>
2025-09-11 11:50:56 +0300

    Added Contact Controller.
    Added Create, Update and Delete logics

---
+++ OnlineEducation.UI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OnlineEducation.UI.DTOs.ContactDtos;
+using OnlineEducation.UI.Helpers;
+
+namespace OnlineEducation.UI.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class ContactController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+   
+        public async Task<IActionResult> Index()
+        {
+
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+        }
+
+        public ActionResult CreateContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+
+            await _client.PostAsJsonAsync("contacts", createContactDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateContact(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateContactDto>($"contacts/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            var values = await _client.PutAsJsonAsync("contacts", updateContactDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            await _client.DeleteAsync($"contacts/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
