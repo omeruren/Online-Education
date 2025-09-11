5303baea529d135c3105d6c792a822a2b113b089
omeruren <omer.uren19@gmail.com>
2025-09-11 21:35:49 +0300

    UI Layout Added

---
+++ OnlineEducation.UI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEducation.UI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult Layout()
+        {
+            return View();
+        }
+    }
+}
