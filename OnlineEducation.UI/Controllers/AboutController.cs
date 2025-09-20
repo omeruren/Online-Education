cdaaa77fc191446bfca90df33cabafbfe5d4d8f6
omeruren <omer.uren19@gmail.com>
2025-09-20 21:37:28 +0300

    About UI

---
+++ OnlineEducation.UI/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.UI.DTOs.AboutDtos;
+using OnlineEducation.UI.Helpers;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
+            return View(values);
+        }
+    }
+}
