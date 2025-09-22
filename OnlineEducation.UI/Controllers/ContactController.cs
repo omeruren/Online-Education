aa142263a4ced70e15e2f563ad6d9118cd8742c4
omeruren <omer.uren19@gmail.com>
2025-09-22 12:14:18 +0300

    Contact Component
    Edited Footer Section

---
+++ OnlineEducation.UI/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.UI.DTOs.MessageDtos;
+using OnlineEducation.UI.Helpers;
+
+namespace OnlineEducation.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+        {
+            await _client.PostAsJsonAsync("messages", createMessageDto);
+            return NoContent();
+        }
+    }
+}
---
+++ OnlineEducation.UI/ViewComponents/UILayout/_UILayoutSocialMediaLinks.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.UI.DTOs.SocialMediaDtos;
+using OnlineEducation.UI.Helpers;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.UI.ViewComponents.UILayout
+{
+    public class _UILayoutSocialMediaLinks : ViewComponent
+    {
+        private readonly  HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var socialLinks = await _client.GetFromJsonAsync<List<ResultSocialMediaDto>>("socialMedias");
+            return View(socialLinks);
+        }
+    }
+}
