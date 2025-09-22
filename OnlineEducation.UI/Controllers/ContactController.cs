cd0a561acbcbbc43d40bfb1a35594ba9feeed0c0
omeruren <omer.uren19@gmail.com>
2025-09-22 12:58:46 +0300

    Contact Controller and Contact ınfo Component

--- OnlineEducation.UI/Controllers/ContactController.cs
+++ OnlineEducation.UI/Controllers/ContactController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.UI.DTOs.ContactDtos;
 using OnlineEducation.UI.DTOs.MessageDtos;
 using OnlineEducation.UI.Helpers;
+using System.Threading.Tasks;
 
 namespace OnlineEducation.UI.Controllers
 {
@@ -8,8 +10,10 @@ namespace OnlineEducation.UI.Controllers
     {
         private readonly HttpClient _client = HttpClientInstance.CreateClient();
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var mapUrls = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            ViewBag.MapUrls = mapUrls.Select(x => x.MapUrl).FirstOrDefault();
             return View();
         }
         [HttpPost]
@@ -18,6 +22,12 @@ namespace OnlineEducation.UI.Controllers
         {
             await _client.PostAsJsonAsync("messages", createMessageDto);
             return NoContent();
+        }
+
+        public async Task<PartialViewResult> ContactMap()
+        {
+           
+            return PartialView();
         }
     }
 }
