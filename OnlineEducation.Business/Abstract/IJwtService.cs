6ab8dea4503992522e75ba0d5211b7a31cca3ae4
omeruren <omer.uren19@gmail.com>
2025-09-23 18:08:59 +0300

    JWT :  configurations

---
+++ OnlineEducation.Business/Abstract/IJwtService.cs
@@ -0,0 +1,15 @@
+﻿using OnlineEducation.DTO.DTOs.LoginDtos;
+using OnlineEducation.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.Business.Abstract
+{
+    public interface IJwtService
+    {
+        Task<LoginResponseDto> CreateTokenAsync(AppUser user);
+    }
+}
