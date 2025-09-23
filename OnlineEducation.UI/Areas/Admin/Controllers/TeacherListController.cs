50e6355cbdbf5179382133bc128c90c80adf7208
omeruren <omer.uren19@gmail.com>
2025-09-23 13:08:55 +0300

    Teacher List Controller

---
+++ OnlineEducation.UI/Areas/Admin/Controllers/TeacherListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Entity.Entities;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.UI.Areas.Admin.Controllers
+{
+    public class TeacherListController(UserManager<AppUser> _userManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var teachers = await _userManager.GetUsersInRoleAsync("Teacher");
+            return View(teachers);
+        }
+    }
+}
